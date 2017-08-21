05ef1db19bc11b347eca8515d7eb0b22b374bc87
Jess Tate <jess@sci.utah.edu>
2017-08-20 23:16:48 -0600

    fixed a syntax error

--- Python/scoreCommon.py
+++ Python/scoreCommon.py
@@ -136,7 +136,7 @@ def computePOT(truthMatrix, testMatrix):
     for i in range(n):
         truthVec=(truthMatrix[:,i])
         testVec=(testMatrix[:,i])
-		if (truthVec != truthVec[0]) & (testVec != testVec[0])
+	    if (truthVec != truthVec[0]) & (testVec != testVec[0]):
         	sumCorrelation= sumCorrelation + scipy.stats.pearsonr(truthVec, testVec)[0]
         Error_t=math.pow(np.linalg.norm(truthVec-testVec),2)
         magtruthVect = magnitudesqure(truthVec)
